876d1a98cc61aa94d3e15d6ddbe565ee97ba18f0
VictorSa <victor.rsa@mercadolivre.com>
2022-11-30 11:20:20 -0300

    Finalizando model / Victor

--- src/main/java/com/grupo6/buntuconectapp/model/Tema.java
+++ src/main/java/com/grupo6/buntuconectapp/model/Tema.java
@@ -3,12 +3,15 @@ package com.grupo6.buntuconectapp.model;
 
 
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Entity
-@Table(name = "tb_temas")
+@Table(name = "tb_tema")
 public class Tema {
 
     @Id
@@ -23,6 +26,11 @@ public class Tema {
     @Size(min = 4, max = 20, message = "O nome do tema deve conter de 4 a 20 caracteres ")
     @NotBlank(message = "O Nome é obrigatorio !")
     private String nome;
+
+
+    @OneToMany(mappedBy = "tema", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties("Tema")
+    private List<Postagem> postagem;
 
 
     public Long getId() {
@@ -47,5 +55,13 @@ public class Tema {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public List<Postagem> getPostagem() {
+        return postagem;
+    }
+
+    public void setPostagem(List<Postagem> postagem) {
+        this.postagem = postagem;
     }
 }
